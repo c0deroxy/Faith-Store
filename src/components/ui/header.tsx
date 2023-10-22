750e61be662d1bd92a9f080aaf324cdb168d47fc
João Luis <howl.github@gmail.com>
2023-10-22 12:23:42 -0700

    feat: add auth handler to header component

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -1,7 +1,10 @@
+"use client";
+
 import {
   HomeIcon,
   ListOrderedIcon,
   LogInIcon,
+  LogOutIcon,
   MenuIcon,
   PercentCircleIcon,
   PercentIcon,
@@ -10,8 +13,21 @@ import {
 import { Button } from "./button";
 import { Card } from "./card";
 import { Sheet, SheetContent, SheetHeader, SheetTrigger } from "./sheet";
+import { signIn, signOut, useSession } from "next-auth/react";
+import { Avatar, AvatarFallback, AvatarImage } from "./avatar";
+import { Separator } from "./separator";
 
 const Header = () => {
+  const { data, status } = useSession();
+
+  const handleLoginClick = async () => {
+    await signIn("google");
+  };
+
+  const handleLogoutClick = async () => {
+    await signOut();
+  };
+
   return (
     <Card className="flex items-center justify-between p-[1.875em]">
       <Sheet>
@@ -25,11 +41,35 @@ const Header = () => {
             Menu
           </SheetHeader>
 
-          <div className="mt-2 flex flex-col gap-3">
-            <Button variant="outline" className="w-full justify-start gap-2">
-              <LogInIcon size={16} />
-              Fazer Login
-            </Button>
+          {status == "authenticated" && data.user && (
+            <div className="flex flex-col">
+              <div className="flex items-center gap-2 py-4">
+                <Avatar>
+                  <AvatarFallback>
+                    {data.user.name?.toUpperCase()}
+                  </AvatarFallback>
+
+                  {data.user.image && <AvatarImage src={data.user.image} />}
+                </Avatar>
+
+                <p className="font-medium">{data.user.name}</p>
+              </div>
+
+              <Separator />
+            </div>
+          )}
+
+          <div className="mt-4 flex flex-col gap-3">
+            {status == "unauthenticated" && (
+              <Button
+                onClick={handleLoginClick}
+                variant="outline"
+                className="w-full justify-start gap-2"
+              >
+                <LogInIcon size={16} />
+                Fazer Login
+              </Button>
+            )}
 
             <Button variant="outline" className="w-full justify-start gap-2">
               <HomeIcon size={16} />
@@ -45,6 +85,17 @@ const Header = () => {
               <ListOrderedIcon size={16} />
               Catalogo
             </Button>
+
+            {status == "authenticated" && (
+              <Button
+                onClick={handleLogoutClick}
+                variant="outline"
+                className="w-full justify-start gap-2"
+              >
+                <LogOutIcon size={16} />
+                Sair da conta
+              </Button>
+            )}
           </div>
         </SheetContent>
       </Sheet>
