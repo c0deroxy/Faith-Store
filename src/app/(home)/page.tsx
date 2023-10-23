50a5bdc4feffdcd7269183302906217e2c63e7bc
João Luis <howl.github@gmail.com>
2023-10-23 06:45:09 -0700

    feat: add first discount banner to home page

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { useSession } from "next-auth/react";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="p-5">
+      <Image
+        src="/banner-55off.png"
+        width={0}
+        height={0}
+        sizes="100vw"
+        className="h-auto w-full"
+        alt="Até 55% de desconto esse mês"
+      />
+    </div>
+  );
+}
