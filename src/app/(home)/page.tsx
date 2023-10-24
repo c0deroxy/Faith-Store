11854f04d8dc6e6b69e9d7b1978256c4f7fa617b
João Luis <howl.github@gmail.com>
2023-10-24 13:04:23 -0700

    feat: keyboard section

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,8 +1,10 @@
 import { useSession } from "next-auth/react";
 import Image from "next/image";
-import Categories from "./components/categories";
 import { prismaClient } from "@/lib/prisma";
 import ProductList from "./components/productList";
+import SectionTitle from "./components/sectionTitle";
+import Categories from "./components/categories";
+import PromoBanner from "./components/promoBanner";
 
 export default async function Home() {
   const deals = await prismaClient.product.findMany({
@@ -12,15 +14,20 @@ export default async function Home() {
       },
     },
   });
+
+  const keyboard = await prismaClient.product.findMany({
+    where: {
+      category: {
+        slug: "keyboards",
+      },
+    },
+  });
+
   return (
     <div>
-      <Image
+      <PromoBanner
         src="/banner-55off.png"
-        width={0}
-        height={0}
-        sizes="100vw"
-        className="h-auto w-full px-5"
-        alt="Até 55% de desconto esse mês"
+        alt="Até 55% de desconto em mouses"
       />
 
       <div className="mt-8 px-5">
@@ -28,18 +35,19 @@ export default async function Home() {
       </div>
 
       <div className="mt-8">
-        <p className="mb-2 pl-5 font-semibold uppercase">Ofertas</p>
+        <SectionTitle>Ofertas</SectionTitle>
         <ProductList products={deals} />
       </div>
 
-      <Image
+      <PromoBanner
         src="/banner-mouses.png"
-        width={0}
-        height={0}
-        sizes="100vw"
-        className="h-auto w-full px-5"
         alt="Até 55% de desconto em mouses"
       />
+
+      <div className="mt-8">
+        <SectionTitle>Teclados</SectionTitle>
+        <ProductList products={keyboard} />
+      </div>
     </div>
   );
 }
