0fcba2337120944292b4845fb303b77dc8bc9c1f
João Luis <howl.github@gmail.com>
2023-10-24 12:50:49 -0700

    feat: add second home page discount banner

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -28,8 +28,18 @@ export default async function Home() {
       </div>
 
       <div className="mt-8">
+        <p className="mb-2 pl-5 font-semibold uppercase">Ofertas</p>
         <ProductList products={deals} />
       </div>
+
+      <Image
+        src="/banner-mouses.png"
+        width={0}
+        height={0}
+        sizes="100vw"
+        className="h-auto w-full px-5"
+        alt="Até 55% de desconto em mouses"
+      />
     </div>
   );
 }
